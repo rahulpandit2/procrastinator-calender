b906135cfa99656e672a8bc4b7cddc638cc20f9b
Rahul Pandit <92500570+rahulpandit2@users.noreply.github.com>
2023-11-15 20:50:28 +0530

    added the basic html file, css file, js file. In the current version the user is able to enter his date of birth with the only restriction being that the range of date can not be more then 90 years or in future. Last 10 values entered by the user is saved the user has the option to delete the saved data the user's data in also saved in the cache so that the user doesn't have to re enter the data eveytime he refreshes the page user also has the option to delete the data from the cache

---
+++ js/main.js
@@ -0,0 +1,115 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Load the last user input
+    loadLastUserInput();
+    // Automatically calculate and display results
+    calculateWeeks();
+    // Load recent dates from local storage
+    loadRecentDates();
+});
+
+function calculateWeeks() {
+    const dobInput = document.getElementById("dobInput");
+    const lifeGrid = document.getElementById("lifeGrid");
+    const errorDiv = document.getElementById("error");
+
+    const dob = new Date(dobInput.value);
+    const currentDate = new Date();
+    const maxAllowedDate = new Date(currentDate.getFullYear() - 90, currentDate.getMonth(), currentDate.getDate());
+
+    if (isNaN(dob.getTime()) || dob > currentDate || dob < maxAllowedDate) {
+        errorDiv.textContent = "Please enter a valid date of birth between 90 years ago and today.";
+        return;
+    }
+
+    errorDiv.textContent = "";  // Clear any previous error message
+
+    const weeksPassed = Math.floor((currentDate - dob) / (1000 * 60 * 60 * 24 * 7));
+
+    saveDate(dob);
+
+    lifeGrid.innerHTML = "";
+
+    for (let row = 0; row < 60; row++) {
+        for (let col = 0; col < 78; col++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            if (row * 78 + col < weeksPassed) {
+                cell.classList.add("red");
+            } else if (row * 78 + col === weeksPassed) {
+                cell.classList.add("yellow");
+            } else if (row * 78 + col - weeksPassed <= 10) {
+                cell.classList.add("green");
+            }
+
+            lifeGrid.appendChild(cell);
+        }
+    }
+
+    lifeGrid.classList.add("visible");
+    loadRecentDates();
+}
+
+function saveDate(date) {
+    const recentDates = JSON.parse(localStorage.getItem("recentDates")) || [];
+    recentDates.unshift(date.toISOString());
+    // Save the last 10 user inputs
+    const top10Dates = recentDates.slice(0, 10);
+    localStorage.setItem("recentDates", JSON.stringify(top10Dates));
+
+    // Save the current input as the last user input
+    saveLastUserInput(dobInput.value);
+}
+
+function loadRecentDates() {
+    const recentDates = JSON.parse(localStorage.getItem("recentDates")) || [];
+    const recentDatesDropdown = document.getElementById("recentDates");
+    recentDatesDropdown.innerHTML = '<option value="">Select a recent date</option>';
+
+    recentDates.forEach((date) => {
+        const option = document.createElement("option");
+        option.value = date;
+        option.textContent = new Date(date).toLocaleDateString();
+        recentDatesDropdown.appendChild(option);
+    });
+}
+
+function loadSelectedDate() {
+    const selectedDate = document.getElementById("recentDates").value;
+
+    if (selectedDate) {
+        // Convert the selected date to the format expected by the date input
+        const formattedDate = new Date(selectedDate).toISOString().split('T')[0];
+        document.getElementById("dobInput").value = formattedDate;
+        calculateWeeks();
+    }
+}
+
+function saveLastUserInput(input) {
+    localStorage.setItem("lastUserInput", input);
+}
+
+function loadLastUserInput() {
+    const lastUserInput = localStorage.getItem("lastUserInput");
+    if (lastUserInput) {
+        document.getElementById("dobInput").value = lastUserInput;
+    }
+}
+
+function clearHistory() {
+    // Clear the recentDates and lastUserInput from local storage
+    localStorage.removeItem("recentDates");
+    localStorage.removeItem("lastUserInput");
+    // Clear the dropdown options
+    document.getElementById("recentDates").innerHTML = '<option value="">Select a recent date</option>';
+}
+
+function clearResults() {
+    // Clear the lifeGrid and hide it
+    document.getElementById("lifeGrid").innerHTML = "";
+    document.getElementById("lifeGrid").classList.remove("visible");
+
+    // Remove cached data related to results
+    localStorage.removeItem("recentDates");
+    localStorage.removeItem("lastUserInput");
+}
